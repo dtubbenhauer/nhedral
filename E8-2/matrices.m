aac7ff91346dfb5d4df103119b40eb82e2d8f2aa
abellacabanne <56727304+abellacabanne@users.noreply.github.com>
2025-04-08 10:51:48 +0200

    Update matrices.m

--- E8-2/matrices.m
+++ E8-2/matrices.m
@@ -52,6 +52,4 @@ M2:=Matrix(CyclotomicField(96),24,24,[0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 
 
 M3:=Transpose(M1);
 
-D,V:=Diagonalization([M1,M2,M3]);
-
-[[D[j][i][i] : j in [1..3]]: i in [1..24]];
+check_matrices(M1,M2,M3,8);
